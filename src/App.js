4a08753b082066a9a33c6621329449b0e87fa1b3
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-05-11 08:47:03 +0100

    Added container and navbar

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ import home from './pages/home';
 import login from'./pages/login';
 import signup from './pages/signup';
 
+// Components
+import NavBar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar/>
+        <div className="container">
         <Switch>
           <Route exact path="/" component={home} />
           <Route exact path="/login" component={login} />
           <Route exact path="/signup" component={signup} />
         </Switch>
+        </div>
       </Router>
     </div>
   );
