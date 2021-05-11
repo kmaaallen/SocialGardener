d261d513604d33adfcb7f783f2fe02ae7d82b8c0
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-05-11 08:27:37 +0100

    Added routes to pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+// Pages
+import home from './pages/home';
+import login from'./pages/login';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Social Gardener App</h1>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
