e881a9c176b8c14126828e49e0c783a77c329119
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-05-12 08:44:09 +0100

    Updated homepage to show posts

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 
-export class home extends Component {
+import axios from 'axios';
+
+//components
+import Post from '../components/Post';
+
+class home extends Component {
+    state = {
+        posts: null
+    }
+
+    componentDidMount(){
+        axios.get('/posts')
+        .then((response) => {
+            this.setState({
+                posts: response.data
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
     render() {
+        let recentPosts = this.state.posts ? (
+            this.state.posts.map((post) => <Post post={post}/>)
+        ) : <p>Loading ...</p>
         return (
-            <div>
-                <h1>Homepage</h1>
-            </div>
+            <Grid container spacing={3}>
+                <Grid item sm={8} xs={12}>
+                    {recentPosts}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
         )
     }
 }
