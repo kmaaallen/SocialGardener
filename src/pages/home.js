0ec1291527e10547e677f68e05e3ad47ff9aaef3
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-05-21 08:29:05 +0100

    Added profile component

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 //components
 import Post from '../components/Post';
+import Profile from '../components/Profile';
 
 class home extends Component {
     state = {
@@ -33,7 +34,7 @@ class home extends Component {
                     {recentPosts}
                 </Grid>
                 <Grid item sm={4} xs={12}>
-                    <p>Profile...</p>
+                    <Profile />
                 </Grid>
             </Grid>
         )
