f87e86c61d58ffb34ef7e6b269e4fecfcdd97c22
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-05-21 07:31:25 +0100

    Added logout, sign up and helper auth function

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,13 +1,34 @@
 import axios from 'axios';
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types';
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types';
 
 export const loginUser = (userData, history) => (dispatch) => {
     dispatch({ type: LOADING_UI });
     axios.post('/login', userData)
         .then((response) => {
-            const FBIdToken = `Bearer ${response.data.token}`;
-            localStorage.setItem('FBIdToken', FBIdToken);
-            axios.defaults.headers.common['Authorization'] = FBIdToken;
+            setAuthorizationHeader(response.data.token);
+            dispatch(getUserData());
+            dispatch({ type: CLEAR_ERRORS });
+            history.push('/');
+        })
+        .catch((error) => {
+            dispatch({
+                type: SET_ERRORS,
+                payload: error.response.data
+            })
+        })
+}
+
+export const logoutUser = () => (dispatch) => {
+    localStorage.removeItem('FBIdToken');
+    delete axios.defaults.headers.common['Authorization'];
+    dispatch({ type: SET_UNAUTHENTICATED});
+}
+
+export const signUpUser = (newUserData, history) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+    axios.post('/signup', newUserData)
+        .then((response) => {
+            setAuthorizationHeader(response.data.token);
             dispatch(getUserData());
             dispatch({ type: CLEAR_ERRORS });
             history.push('/');
@@ -28,4 +49,10 @@ export const getUserData = () => (dispatch) => {
         .catch(error => {
             console.error(error);
         })
+}
+
+const setAuthorizationHeader = (token) => {
+    const FBIdToken = `Bearer ${token}`;
+    localStorage.setItem('FBIdToken', FBIdToken);
+    axios.defaults.headers.common['Authorization'] = FBIdToken;
 }
