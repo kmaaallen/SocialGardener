504d3e6114c6fde2f55d0e0a1d2ee3ee5e9875f4
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-05-21 07:29:54 +0100

    Added connect, prop-types

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 const AuthRoute = ({ component: Component, authenticated, ...rest }) => (
     <Route
@@ -8,4 +10,13 @@ const AuthRoute = ({ component: Component, authenticated, ...rest }) => (
     />
 )
 
-export default AuthRoute;
+AuthRoute.propTypes = {
+    user: PropTypes.object
+}
+
+
+const mapStateToProps = (state) => ({
+    authenticated: state.user.authenticated
+})
+
+export default connect(mapStateToProps)(AuthRoute);
