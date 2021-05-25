6c725ccb602426d5c23e9cd7b5bec61ba5f6940e
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-05-25 22:12:23 +0100

    Added logout, hid login and signup when auth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 //Material UI
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
+//Redux
+import { connect } from 'react-redux';
+import { logoutUser } from '../redux/actions/userActions';
+
 export class NavBar extends Component {
     render() {
-        return (
+        const {authenticated} = this.props;
+        let links = !authenticated ? ((
             <AppBar position="fixed">
                 <Toolbar className="toolbar">
-                    <Button color="inherit" component={Link} to="/login">Login</Button>
                     <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component={Link} to="/login">Login</Button>
                     <Button color="inherit" component={Link} to="/signup">Signup</Button>
                 </Toolbar>
             </AppBar>
-        )
+        )) : (
+            <AppBar position="fixed">
+                <Toolbar className="toolbar">
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" onClick={this.props.logoutUser}>Logout</Button>
+                </Toolbar>
+            </AppBar>
+        ) ;
+
+        return links;
     }
 }
 
-export default NavBar
+NavBar.propTypes = {
+    user: PropTypes.object
+}
+
+const mapStateToProps = (state) => ({
+    authenticated: state.user.authenticated
+})
+
+const mapActionsToProps = {
+    logoutUser
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(NavBar);
