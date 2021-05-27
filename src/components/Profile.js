90f61d45c21d5dfeca8d5f045cb60760fdf5b03c
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-05-27 08:29:43 +0100

    Styled profile section and added edit image button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,20 +10,27 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 //Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import Edit from '@material-ui/icons/Edit';
 
 //Redux
 import { connect } from 'react-redux';
+import { uploadImage } from '../redux/actions/userActions';
 
 const styles = {
     image: {
-        height: '100px',
-        width: '100px',
-        marginTop: '20%'
+        width: '180px',
+        height: '180px',
+        borderRadius: '50%',
+        objectFit: 'cover',
+        marginTop: '15px'
     },
+
 
     bio: {
         textAlign: 'center',
@@ -37,6 +44,11 @@ const styles = {
         margin: '15px'
     },
 
+    editButton: {
+        verticalAlign: 'super',
+        padding: 0
+    },
+
     body2: {
         fontSize: '0.875rem',
         verticalAlign: 'super',
@@ -48,15 +60,36 @@ const styles = {
 };
 
 export class Profile extends Component {
+
+    handleEditImage = () => {
+        const fileInput = document.getElementById('imageUpload');
+        fileInput.click();
+    }
+
+    handleImageChange = (event) => {
+        const image = event.target.files[0];
+        const formData = new FormData();
+        formData.append('image', image, image.name);
+        this.props.uploadImage(formData);
+
+
+    }
+
     render() {
         const { classes, user: { credentials: { userName, created, imageUrl, bio, location },
             loading, authenticated } } = this.props;
 
         let profileMarkup = !loading ? (authenticated ? (
-            <Paper>
+            <Paper className={classes.bio}>
                 <Grid container>
-                    <Grid item>
+                    <Grid item xs={12}>
                         <img className={classes.image} src={imageUrl} alt="user profile image"></img>
+                        <input type="file" id="imageUpload" hidden="hidden" onChange={this.handleImageChange} />
+                        <Tooltip title="Upload profile picture" placement="top">
+                            <IconButton className={classes.editButton} onClick={this.handleEditImage}>
+                                <Edit color="primary" />
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
                     <Grid item xs={12} sm container className={classes.padding}>
                         <Grid item xs container direction="column" spacing={2}>
@@ -78,11 +111,11 @@ export class Profile extends Component {
                         </Grid>
                     </Grid>
                     <Grid item xs={12} className={classes.bio}>
-                    {bio && <Typography variant="body2" className={classes.padding}>{bio}</Typography>}
+                        {bio && <Typography variant="body2" className={classes.padding}>{bio}</Typography>}
 
+                    </Grid>
                 </Grid>
-                </Grid>
-                
+
 
             </Paper>
         ) : (
@@ -101,12 +134,17 @@ export class Profile extends Component {
 
 Profile.propTypes = {
     classes: PropTypes.object.isRequired,
-    user: PropTypes.object
+    user: PropTypes.object,
+    uploadImage: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
     user: state.user
 });
 
+const mapActionsToProps = {
+    uploadImage
+}
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile));
