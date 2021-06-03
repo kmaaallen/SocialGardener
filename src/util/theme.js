9cc5a0ecc54360e74d9dc8df91fb7840b0e2fe03
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-06-03 19:05:02 +0100

    moved hr classes to global theme

--- src/util/theme.js
+++ src/util/theme.js
@@ -1,60 +1,70 @@
 export const theme = {
-    palette: {
-      primary: {
-        light: '#aee571',
-        main: '#7cb342',
-        dark: '#4b830d',
-        contrastText: '#000000'
-      },
-      secondary: {
-        light: '#629749',
-        main: '#33691e',
-        dark: '#003d00',
-        contrastText: '#000000'
-      },
+  palette: {
+    primary: {
+      light: '#aee571',
+      main: '#7cb342',
+      dark: '#4b830d',
+      contrastText: '#000000'
     },
-    overrides: {
-      MuiButton: {
-        containedSecondary: {
-          color: '#ffffff'
-        }
-      }
+    secondary: {
+      light: '#629749',
+      main: '#33691e',
+      dark: '#003d00',
+      contrastText: '#000000'
     },
-    classes: {
-      form: {
-        textAlign: 'center',
-      },
-  
-      pageTitle: {
-        margin: '15px 0px',
-      },
-  
-      textField: {
-        margin: '15px 0px',
-      },
-  
-      button: {
-        marginTop: '15px',
-        position: 'relative'
-      },
-  
-      loader: {
-        position: 'absolute',
-      },
-  
-      generalError: {
-        color: 'red',
-        fontSize: '0.8em'
-      },
-  
-      signUpText: {
-        marginTop: '15px',
-        fontSize: '0.8em'
-      },
-  
-      signUpLink: {
-        color: '#33691e',
-        textDecoration: "underline"
+  },
+  overrides: {
+    MuiButton: {
+      containedSecondary: {
+        color: '#ffffff'
       }
     }
+  },
+  classes: {
+    form: {
+      textAlign: 'center',
+    },
+
+    pageTitle: {
+      margin: '15px 0px',
+    },
+
+    textField: {
+      margin: '15px 0px',
+    },
+
+    button: {
+      marginTop: '15px',
+      position: 'relative'
+    },
+
+    loader: {
+      position: 'absolute',
+    },
+
+    generalError: {
+      color: 'red',
+      fontSize: '0.8em'
+    },
+
+    signUpText: {
+      marginTop: '15px',
+      fontSize: '0.8em'
+    },
+
+    signUpLink: {
+      color: '#33691e',
+      textDecoration: "underline"
+    },
+
+    invisibleSeparator: {
+      border: 'none',
+      margin: 4
+    },
+    visibleSeparator: {
+      width: '100%',
+      border: '1px solid #ccc',
+      marginBottom: '20px'
+    }
   }
+}
